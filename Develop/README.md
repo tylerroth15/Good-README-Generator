edef7018535f2649ba9184891731871ef8896418
Tyler Roth <tylerroth1535@gmail.com>
2020-06-01 20:54:49 -0400

    Added changed the template literal questions around and added additional input questions. Made the Table of Contents clickable

--- Develop/README.md
+++ Develop/README.md
@@ -1,37 +1,43 @@
 
-# Portfolio
+# Salty-Cloud-Frogs-Project-1
 #### Author: tylerroth15
 
-Description Needed
+An API project that shows our talents to manipulate API keys to create a retro arcade game. 
 
 # Table of Contents
-* ## Installation 
-* ## Usage
-* ## Tests
-* ## Questions
-* ## Contributors
-* ## License
+* [Installation](#installation) 
+* [Usage](#usage)
+* [Tests](#tests)
+* [Questions](#questions)
+* [Contriubtors](#contributors)
+* [License](#license)
 
 ## Installation
-Enter installation instructions.
+
+NA
 
 ## Usage
-Enter Usage Instructions.
+
+undefined
 
 ## Tests
 
+It's interactive
 
 ## Questions
-Using the email link below, please reach out with any questions.
 
-### Email
+Using the email link below, please reach out with any questions.
 
 ### Picture
 <img src="https://avatars0.githubusercontent.com/u/59520608?v=4" width="30" style="border-radius: 15px"> 
 
-## Contributors
+### Email
 
+tylerroth1535@gmail.com
+
+## Contributors
+none
 
 ## License
-<img src="https://img.shields.io/github/license/tylerroth15/Portfolio">
+<img src="https://img.shields.io/github/license/tylerroth15/Salty-Cloud-Frogs-Project-1">
 
--- Develop/index.js
+++ Develop/index.js
@@ -2,21 +2,16 @@ const inquirer = require("inquirer");
 const fs = require('fs');
 const axios = require('axios');
 // const markdown =  require('./utils/generateMarkdown');
-const description = "Description Needed";
-const contributors = [];
-const installation = "Enter installation instructions."; 
-const usage = "Enter Usage Instructions.";
-const tests = "Enter any and all tests run."; 
+// const description = "Description Needed";
+const credits = ["none"]; 
+// const installation = "Enter installation instructions."; 
+// const usage = "Enter Usage Instructions.";
+// const tests = "Enter any and all tests run."; 
 const faq = "Using the email link below, please reach out with any questions.";
-const email = "someone@gmail.com";
+// const email = "someone@gmail.com";
 
 
 const questions = [
-    {
-    type: "input",
-    name: "repo",
-    message: "What is your Repo title?",
-}, 
 {
     type: "input",
     name: "username",
@@ -24,96 +19,54 @@ const questions = [
 },
 {
     type: "input",
-    name: "descrip",
+    name: "repo",
+    message: "What is your Repo title?",
+}, 
+{
+    type: "input",
+    name: "description",
     message:"Write a brief description of your project.",
 },
 {
     type: "input",
-    name: "installation",
-    message: "Briefly describe the installation process for your file.",
+    name: "install",
+    message: "What command should be used to install dependencies?",
 },
 {
     type: "input",
-    name: "usage",
-    message: "What can your project be used for?",
+    name: "tests",
+    message: "What command should be used to run tests?",
 },
-// {
-//     type: "input",
-//     name: "testing",
-//     message: "What is a test that you can do for your project",
-// }
-// Description of Repo
-// const writeDescription = (des)=> {
-//     if (des !=null){
-//         description= des;
-//         writeInstallation();
-//     }else {
-//         inquirer.prompt({
-            
-//             name: "descrip"
-//         }).then(({descrip})=> {
-//             description = descrip;
-//             writeInstallation();
-//         })
-//     }
-// }
-// // Installing of Repo
-// const writeInstallation = () => {
-//     inqurier.prompt({
-//         type: "input",
-//         message: "Briefly describe the installation process for your file.",
-//         name: "install"
-//     }).then(({install})=>{
-//         installation = install;
-//         writeUsage();
-//     });
-// }
-// // Usage of Repo
-// const writeUsage = () =>{
-//     inquirer.prompt({
-//         type: "input",
-//         message: "What can your project be used for?",
-//         name: "uses"
-//     }).then(({uses})=>{
-//     usage = uses
-//     writeTests();
-// })
-// }
-// //Testing for Repo
-// const writeTest =() =>
-// inquirer.prompt({
-//     type: "input",
-//     message: "What is a test that you can do for your project",
-//     name: "test"
-// }).then(({test})=>{
-//     tests= test;
-//     writeQuestions();
-// });
-
+{
+    type: "input",
+    name: "tests",
+    message: "What should a user know about installing this repo?"
+},
+{
+    type: "input",
+    name: "email",
+    message: "Please enter your email address."
+}
 ];
+
 inquirer.prompt(questions)
-.then(({username, repo}) =>{
+.then(({username, repo, description, install, tests, usage, email}) =>{
     const queryUrl = `https://api.github.com/repos/${username}/${repo}`;
         const contribUrl = `https://api.github.com/repos/${username}/${repo}/contributors`;
         axios
 
     .get(queryUrl)
     .then(res =>{
-        if (res.data.description != null){
-            description = res.data.description;
-        }
-
         axios
         .get(contribUrl)
         .then(data => {
+            for (let i=0; i < data.data.length; i++){
+                credits.push(data.data[i].login);
+                console.log(credits);
 
-            data.data.forEach(element => {
-                contributors.push(element.login);
+            }
                 
-            })
-        });
-        
-
+        })
         const readme =
 `
 # ${repo}
@@ -122,32 +75,38 @@ inquirer.prompt(questions)
 ${description}
 
 # Table of Contents
-* ## Installation 
-* ## Usage
-* ## Tests
-* ## Questions
-* ## Contributors
-* ## License
+* [Installation](#installation) 
+* [Usage](#usage)
+* [Tests](#tests)
+* [Questions](#questions)
+* [Contriubtors](#contributors)
+* [License](#license)
 
 ## Installation
-${installation}
+
+${install}
 
 ## Usage
+
 ${usage}
 
 ## Tests
 
+${tests}
 
 ## Questions
-${faq}
 
-### Email
+${faq}
 
 ### Picture
 <img src="${res.data.owner.avatar_url}" width="30" style="border-radius: 15px"> 
 
+### Email
+
+${email}
+
 ## Contributors
-${contributors}
+${credits}
 
 ## License
 <img src="https://img.shields.io/github/license/${username}/${repo}">
